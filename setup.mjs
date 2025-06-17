8e9ce83b4783217eaf7a0273ab4b831761df3a12
Alexandre Agostini <a.agostini.fr@gmail.com>
2025-06-18 00:38:24 +0200

    v2.1.148 - fixes

--- setup.mjs
+++ setup.mjs
@@ -57,7 +57,7 @@
 // Stage 32 - Cartography Paper Please!
 
 // --- Configuration ---
-const MOD_VERSION = "v2.1.147";
+const MOD_VERSION = "v2.1.148";
 
 // --- Module Imports ---
 let mModules = null;
